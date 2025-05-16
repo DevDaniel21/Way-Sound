fa5f4d0c03f4f15c638304b81486d66dfc7393f1
rafael-staine <rafael.staine@aluno.ifsp.edu.br>
2025-05-16 15:16:17 -0300

    :guitar: Atualizando pagina inicial e pesquisa :notes:

--- src/js/semConta.js
+++ src/js/semConta.js
@@ -1,31 +1,65 @@
-let conta = false
+let conta = false;
 let contaLogada = conta;
 
 //  ABRIR ALERTA PARA LOGAR CONTA
 if (!contaLogada) {
-    const lightbox = document.getElementById('alerta-conta');
-    const lightboxFechar = document.getElementById('lightboxFechar');
-    
-    // ABRIR ALERTA
-    const linkConta = document.querySelectorAll('.linkConta');
-    linkConta.forEach( ( link ) => {
-        link.addEventListener('click', function () {
-            lightbox.classList.add('active');
-        })
-    })
-    
-    // ABRIR ALERTA MUSICAS
-    const musicasList = document.querySelectorAll('.musicas-card');
-    console.log(musicasList)
-    musicasList.forEach( ( musica ) => {
-        musica.addEventListener('click', function () {
-            lightbox.classList.add('active');
-        })
-    })
-    
-    // BOTAO DE FECHAR
-    lightboxFechar.addEventListener('click', function () {
-        lightbox.classList.remove('active');
-    })
+  const lightbox = document.getElementById("alerta-conta");
+  const lightboxFechar = document.getElementById("lightboxFechar");
 
+  // ABRIR ALERTA
+  const linkConta = document.querySelectorAll(".linkConta");
+  linkConta.forEach((link) => {
+    link.addEventListener("click", function () {
+      lightbox.classList.add("active");
+    });
+  });
+
+  // ABRIR ALERTA MUSICAS
+  // const musicasList = document.querySelectorAll(".musicas-card");
+  // console.log(musicasList);
+  // musicasList.forEach((musica) => {
+  //   musica.addEventListener("click", function () {
+  //     lightbox.classList.add("active");
+  //   });
+  // });
+
+  // BOTAO DE FECHAR
+  lightboxFechar.addEventListener("click", function () {
+    lightbox.classList.remove("active");
+  });
+
+  // ABRIR ALERTA - BOTÃO DAS MÚSICAS
+  const cardButtons = document.querySelectorAll(".card-button");
+  cardButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      lightbox.classList.add("active");
+    });
+  });
 }
+
+// NÃO ESTAVA DANDO CERTO LINKAR O JS (MAIN) NO HTML DE PESQUISA, ENTÃO COLOQUEI O JS AQUI MESMO
+
+//BOTÃO PLAY E PAUSE
+const buttons = document.querySelectorAll(".btn-toggle");
+buttons.forEach((button) => {
+  const icon = button.querySelector(".icon-toggle");
+
+  button.addEventListener("click", () => {
+    if (icon.name === "play-outline") {
+      icon.name = "pause-outline";
+    } else {
+      icon.name = "play-outline";
+    }
+  });
+});
+
+// BARRA DE PESQUISA
+document.getElementById("pesquisaInput").addEventListener("input", function () {
+  const filtro = this.value.toLowerCase();
+  const cards = document.querySelectorAll(".card-music");
+
+  cards.forEach((card) => {
+    const titulo = card.querySelector(".text-music").textContent.toLowerCase();
+    card.style.display = titulo.includes(filtro) ? "" : "none";
+  });
+});
