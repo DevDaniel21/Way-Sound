abf569d71c19b493c09fecfc61914c2590872a6f
DevDaniel21 <danielglaucia2.007@gmail.com>
2025-06-03 20:25:45 -0300

    Pasta organizada, em frontend e backend

--- src/js/models/Musica.model.js
+++
@@ -1,9 +0,0 @@
-export class MusicaModel {
-  constructor(nome, artista, foto, audio, id = Date.now()) {
-    this.id = id;
-    this.nome = nome;
-    this.artista = artista;
-    this.foto = foto;
-    this.audio = audio;
-  }
-}
