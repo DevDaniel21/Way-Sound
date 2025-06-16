6473b4f6b0f5af8792c261b4d1309b3d8d0be4f7
matheus-Bugatti <campos.n@aluno.ifsp.edu.br>
2025-06-16 17:02:08 -0300

    carrossel-artistas

--- docs/src/js/artistas-mainpage.js
+++ docs/src/js/artistas-mainpage.js
@@ -16,21 +16,24 @@ document.addEventListener('DOMContentLoaded', () => {
                 [artists[i], artists[j]] = [artists[j], artists[i]];
             }
 
+            // Limitar a 10 artistas
+            const artistasExibir = artists.slice(0, 13);
+
             // Clear loading state (if any)
             artistasList.innerHTML = '';
 
             // Loop through artists and create <li> elements
-            artists.forEach(artist => {
+            artistasExibir.forEach(artist => {
                 const li = document.createElement('li');
                 li.className = 'artistas-card';
                 li.innerHTML = `
-            <figure 
-              style="background-image: url(${artist.avatar});" 
-              class="artistas-card-img">
-            </figure>
-            <p class="artistas-card-titulo">${artist.nome}</p>
-            <p class="artistas-card-subtitulo">Artista</p>
-          `;
+                    <figure 
+                      style="background-image: url(${artist.avatar});" 
+                      class="artistas-card-img">
+                    </figure>
+                    <p class="artistas-card-titulo">${artist.nome}</p>
+                    <p class="artistas-card-subtitulo">Artista</p>
+                `;
                 artistasList.appendChild(li);
             });
         })
