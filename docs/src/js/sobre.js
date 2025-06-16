857cabeefbdf305c848dfc8665500e3bbd777eb6
Jhonautas <cassio.cruz@aluno.ifsp.edu.br>
2025-06-16 14:40:34 -0300

    :notes: Arrumando adm page(mudando nome do index e tirando opções da tabela) e sobre-nos :notes:

--- docs/src/js/sobre.js
+++ docs/src/js/sobre.js
@@ -1,34 +1,34 @@
 const membros = {
     rafael: {
-        nome: "Rafael Almeida",
+        nome: "Rafael Staine",
         funcao: "Front-end Developer",
         email: "rafael@email.com",
         desc: "Responsável pela interface visual e usabilidade do projeto.",
         img: "https://i.pinimg.com/736x/b7/98/27/b798278394fcfeabbc9d65a38d8f996e.jpg"
     },
     daniel: {
-        nome: "Daniel Silva",
-        funcao: "Back-end Developer",
+        nome: "Daniel Custoodio Santana",
+        funcao: "Full-Stack",
         email: "daniel@email.com",
         desc: "Cuida da lógica de dados, banco e integrações.",
         img: "https://i.pinimg.com/736x/d5/5f/42/d55f42c1edee2c7f60b803a51f0f17df.jpg"
     },
     ws: {
-        nome: "WeSlley Rocha",
+        nome: "WeSlley dA Paz Souza",
         funcao: "Gerente de Projeto",
         email: "weslley@email.com",
         desc: "Organiza as entregas e define o escopo do projeto.",
         img: "../../imgs/logos/logo-pequena.png"
     },
     cassio: {
-        nome: "Cássio Mendes",
+        nome: "Cássio Jhonatan",
         funcao: "UX Designer",
         email: "cassio@email.com",
         desc: "Foca na experiência do usuário e nas interações.",
         img: "https://i.pinimg.com/736x/56/e1/a5/56e1a5c5bf14727f65c39842a3ac9ba0.jpg"
     },
     matheus: {
-        nome: "Matheus Santos",
+        nome: "Matheus Bugatti",
         funcao: "QA Tester",
         email: "matheus@email.com",
         desc: "Testa e garante a qualidade das funcionalidades.",
